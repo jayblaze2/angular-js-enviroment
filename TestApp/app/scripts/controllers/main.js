4d1591469331550d6994831e21fe8212800689f9
OpenShift guest <craig.akers+c9@cinciakers.com>
2013-07-26 17:37:30 -0400

    generate angular app

---
+++ TestApp/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('TestAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
